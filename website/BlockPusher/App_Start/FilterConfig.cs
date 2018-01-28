79f668cd3ceed2861b12aca7d537dff50503f887
alexgermann <alexgermann@github.com>
2018-01-28 14:58:33 -0700

    First crack at website. Implemented login/register with local db.

---
+++ website/BlockPusher/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BlockPusher
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
