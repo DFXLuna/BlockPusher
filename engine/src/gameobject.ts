eec0f2605377c448eafc83b097e44472e0d7c1d8
Matt Grant <teamuba@gmail.com>
2018-04-03 16:56:26 -0600

    Base for game object

--- engine/src/gameobject.ts
+++ engine/src/gameobject.ts
@@ -1,5 +1,42 @@
-export class GameObject {
+export namespace GameObjectManager {
+    let gameObjects: Set< GameObject > = new Set< GameObject >();
 
-    public constructor(){}
+    export function addGameObject( go: GameObject ): void {
+        gameObjects.add( go );
+    }
+
+    export function removeGameObject( go: GameObject ): void {
+        gameObjects.delete( go );
+    }
+    // These might be better as 'getGameObjects' and letting otherstuff
+    // mess with it.
+    export function updateGameObjects(): void {
+        for( let g of gameObjects ){
+            g.update();
+        }
+    }
+
+    export function renderGameObjects(): void {
+        for( let g of gameObjects ){
+            g.render();
+        }
+    }
+}
+
+export class GameObject {
+    protected xPos: number;
+    protected yPos: number;
+
+    public constructor() {
+        this.xPos = 0;
+        this.yPos = 0;
+        GameObjectManager.addGameObject( this );
+    }
+
     public update(): void{}
+    // Possibly unneeded / wrong
+    public render(): void{}
+    public destroy(): void {
+        GameObjectManager.removeGameObject( this );
+    }
 }
