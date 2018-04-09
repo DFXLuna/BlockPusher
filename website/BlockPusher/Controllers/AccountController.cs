7be8807775d936f1c5b49d853f69e08e4834e4b7
alexgermann <alexgermann@github.com>
2018-04-08 21:19:15 -0600

    Added connection string to Azure database. Re-configured accounts to ask for and store name upon registering.

--- website/BlockPusher/Controllers/AccountController.cs
+++ website/BlockPusher/Controllers/AccountController.cs
@@ -163,7 +163,7 @@ namespace BlockPusher.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Hometown = model.Hometown };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -379,7 +379,7 @@ namespace BlockPusher.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Hometown = model.Hometown };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
