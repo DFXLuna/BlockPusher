5378ba938299e8e654867308dde964df02fb9c3f
Adam Coggeshall <dameerkat@gmail.com>
2018-04-11 14:04:25 -0600

    Minor change to test world script.

--- website/BlockPusher/Content/AssetTest/World.js
+++ website/BlockPusher/Content/AssetTest/World.js
@@ -1,10 +1,6 @@
 World.createBlockType("a", "test.png");
 World.createBlockType("b", "test2.jpg");
 
-/*for (var x = 0; x < 15; x++)
-    for (var y = 0; y < 15; y++)
-        World.setBlockTypeAt(x, y, (x + y) % 2 == 0 ? "a" : "b");*/
-
 Render.blockScale = 32
 
 World.update = function () {
